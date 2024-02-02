ebd1468a20691ceb14d0ed5f0f0e573f4b390f08
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2024-02-02 09:12:07 +0100

    Update hanoi.v

--- hanoi.v
+++ hanoi.v
@@ -1,122 +1,159 @@
 module hanoi
-	#(parameter S = 4)
-	(
-	input clk,
-	input rst,
-	input [1:0] fr,
-	input [1:0] to
-	);
-	
-	logic [$clog2(S)-1:0] stick0;
-	logic [$clog2(S)-1:0] stick1;
-	logic [$clog2(S)-1:0] stick2;
-	
-	logic [$clog2(S)-1:0] cnt0;
-	logic [$clog2(S)-1:0] cnt1;
-	logic [$clog2(S)-1:0] cnt2;
-	
-	logic [1:0] f;
-	logic [1:0] t;
-	
-	logic [1:0] tmp0;
-	logic [1:0] tmp1;
-	logic [1:0] tmp2;
-	
-	always_ff @(posedge clk)
-		begin
-			if(rst)
-				begin
-					cnt0 <= '1;
-					cnt1 <= '0;
-					cnt2 <= '0;
-					
-					stick0 <= '1;
-					stick1 <= '0;
-					stick2 <= '0;
-					f <= '0;
-					t <= '0;
-						
-				end
-			else
-				begin
-					f <= fr;
-					t <= to;
-				
-					if(f == "00" && t == "01")							
-						cnt0 <= cnt0 - 1'b1; //smanjujem brojac od stapa0
-						tmp0 <= stick0[cnt0+1]; //vrednost tmp0  koja se prenosi
-											
-						cnt1 <= cnt1 + 1'b1; // povecavam brojac od stapa1
-						stick1[cnt1] <= tmp0; //upisujem na poziciju cnt1 vrednost tmp0
-						
-					
-					if(f == "00" && t == "10")							
-						cnt0 <= cnt0 - 1'b1; //smanjujem brojac od stapa0
-						tmp0 <= stick0[cnt0+1]; //vrednost tmp0  koja se prenosi
-											
-						cnt2 <= cnt2 + 1'b1; // povecavam brojac od stapa2
-						stick2[cnt2] <= tmp0; //upisujem na poziciju cnt2 vrednost tmp0
-						
-					if(f == "01" && t == "00")							
-						cnt1 <= cnt1 - 1'b1; //smanjujem brojac od stapa1
-						tmp1 <= stick1[cnt1+1]; //vrednost tmp1  koja se prenosi
-											
-						cnt0 <= cnt0 + 1'b1; // povecavam brojac od stapa0
-						stick0[cnt0] <= tmp1; //upisujem na poziciju cnt0 vrednost tmp1
-						
-					if(f == "01" && t == "10")						
-						cnt1 <= cnt1 - 1'b1; //smanjujem brojac od stapa1
-						tmp1 <= stick1[cnt1+1]; //vrednost tmp1  koja se prenosi
-											
-						cnt2 <= cnt2 + 1'b1; // povecavam brojac od stapa2
-						stick2[cnt2] <= tmp1; //upisujem na poziciju cnt2 vrednost tmp1
-						
-					if(f == "10" && t == "00")
-						cnt2 <= cnt2 - 1'b1; //smanjujem brojac od stapa2
-						tmp2 <= stick2[cnt2+1]; //vrednost tmp2  koja se prenosi
-											
-						cnt0 <= cnt0 + 1'b1; // povecavam brojac od stapa0
-						stick0[cnt0] <= tmp2; //upisujem na poziciju cnt0 vrednost tmp1
-						
-					if(f == "10" && t == "01")						
-						cnt2 <= cnt2 - 1'b1; //smanjujem brojac od stapa2
-						tmp2 <= stick2[cnt2+1]; //vrednost tmp2  koja se prenosi
-											
-						cnt1 <= cnt1 + 1'b1; // povecavam brojac od stapa1
-						stick1[cnt1] <= tmp2; //upisujem na poziciju cnt1 vrednost tmp2					
-				end
-		end
-	
-	logic legal;
-	always_comb begin	
-		legal = 1'b0; //kada je legal=0, potez je invalid. kada je legal=1, potez je validan.
-		if (f == "11" || t == "11") legal = 1'b0;
-		if (f == "01" && t == "01") legal = 1'b0;
-		if (f == "10" && t == "10") legal = 1'b0;
-		if (f == "00" && t == "01" && (tmp0<tmp1)) legal = 1'b1; //disk koji se prenosi (tmp0) ne sme biti veci od diska na koji se prenosi (tmp1)
-		if (f == "00" && t == "01" && (tmp0>tmp1)) legal = 1'b0;
-		if (f == "00" && t == "10" && (tmp0<tmp2)) legal = 1'b1;
-		if (f == "00" && t == "10" && (tmp0>tmp2)) legal = 1'b0;
-		if (f == "01" && t == "00" && (tmp1<tmp0)) legal = 1'b1;
-		if (f == "01" && t == "00" && (tmp1>tmp0)) legal = 1'b0;
-		if (f == "01" && t == "10" && (tmp1<tmp2)) legal = 1'b1;
-		if (f == "01" && t == "10" && (tmp1>tmp2)) legal = 1'b0;
-		if (f == "10" && t == "00" && (tmp2<tmp0)) legal = 1'b1;
-		if (f == "10" && t == "00" && (tmp2>tmp0)) legal = 1'b0;
-		if (f == "10" && t == "01" && (tmp2<tmp1)) legal = 1'b1;
-		if (f == "10" && t == "01" && (tmp2>tmp1)) legal = 1'b0;
-	end
+#(
+    parameter S = 4,
+    parameter PEG_WIDTH = $clog2(S)-1
+)
+(
+    input clk,
+    input rst,
+    input [1:0] fr,
+    input [1:0] to
+);
 
-	default clocking
-		@(posedge clk);
-	endclocking
-	
-	default disable iff(rst);
-	
-	assume_legal_move: assume property(legal == 1'b1);
-	cover_cnt2: cover property(cnt2 == '1);
-	cover_cnt0_empty: cover property(cnt0 == '0);
-	cover_cnt1_empty: cover property(cnt1 == '0);
-	cover_cnt2_empty: cover property(cnt2 == '0);
+typedef enum logic [3:0] {
+    IDLE,
+    MOVE_FROM_PEG0_TO_PEG1,
+    MOVE_FROM_PEG0_TO_PEG2,
+    MOVE_FROM_PEG1_TO_PEG0,
+    MOVE_FROM_PEG1_TO_PEG2,
+    MOVE_FROM_PEG2_TO_PEG0,
+    MOVE_FROM_PEG2_TO_PEG1
+} State;
+
+logic [PEG_WIDTH:0] peg0;
+logic [PEG_WIDTH:0] peg1;
+logic [PEG_WIDTH:0] peg2;
+
+logic [PEG_WIDTH:0] peg0_count;
+logic [PEG_WIDTH:0] peg1_count;
+logic [PEG_WIDTH:0] peg2_count;
+
+logic [1:0] from_peg;
+logic [1:0] to_peg;
+logic [1:0] tmp_disk;
+
+always_ff @(posedge clk)
+begin
+    State current_state;
+
+    if (rst)
+    begin
+        peg0_count <= '1;
+        peg1_count <= '0;
+        peg2_count <= '0;
+
+        peg0 <= '1;
+        peg1 <= '0;
+        peg2 <= '0;
+
+        from_peg <= '0;
+        to_peg <= '0;
+    end
+    else
+    begin
+        from_peg <= fr;
+        to_peg <= to;
+
+        current_state = IDLE;
+
+        case ({from_peg, to_peg})
+            MOVE_FROM_PEG0_TO_PEG1: current_state = MOVE_FROM_PEG0_TO_PEG1;
+            MOVE_FROM_PEG0_TO_PEG2: current_state = MOVE_FROM_PEG0_TO_PEG2;
+            MOVE_FROM_PEG1_TO_PEG0: current_state = MOVE_FROM_PEG1_TO_PEG0;
+            MOVE_FROM_PEG1_TO_PEG2: current_state = MOVE_FROM_PEG1_TO_PEG2;
+            MOVE_FROM_PEG2_TO_PEG0: current_state = MOVE_FROM_PEG2_TO_PEG0;
+            MOVE_FROM_PEG2_TO_PEG1: current_state = MOVE_FROM_PEG2_TO_PEG1;
+        endcase
+
+        case (current_state)
+            IDLE: begin end
+            default: move_disk(from_peg, to_peg);
+        endcase
+    end
+end
+
+function void move_disk;
+    input [1:0] from, to;
+    begin
+        case ({from, to})
+            MOVE_FROM_PEG0_TO_PEG1:
+                begin
+                    peg0_count <= peg0_count - 1'b1;
+                    tmp_disk <= peg0[peg0_count + 1];
+                    peg1_count <= peg1_count + 1'b1;
+                    peg1[peg1_count] <= tmp_disk;
+                end
+
+            MOVE_FROM_PEG0_TO_PEG2:
+                begin
+                    peg0_count <= peg0_count - 1'b1;
+                    tmp_disk <= peg0[peg0_count + 1];
+                    peg2_count <= peg2_count + 1'b1;
+                    peg2[peg2_count] <= tmp_disk;
+                end
+
+            MOVE_FROM_PEG1_TO_PEG0:
+                begin
+                    peg1_count <= peg1_count - 1'b1;
+                    tmp_disk <= peg1[peg1_count + 1];
+                    peg0_count <= peg0_count + 1'b1;
+                    peg0[peg0_count] <= tmp_disk;
+                end
+
+            MOVE_FROM_PEG1_TO_PEG2:
+                begin
+                    peg1_count <= peg1_count - 1'b1;
+                    tmp_disk <= peg1[peg1_count + 1];
+                    peg2_count <= peg2_count + 1'b1;
+                    peg2[peg2_count] <= tmp_disk;
+                end
+
+            MOVE_FROM_PEG2_TO_PEG0:
+                begin
+                    peg2_count <= peg2_count - 1'b1;
+                    tmp_disk <= peg2[peg2_count + 1];
+                    peg0_count <= peg0_count + 1'b1;
+                    peg0[peg0_count] <= tmp_disk;
+                end
+
+            MOVE_FROM_PEG2_TO_PEG1:
+                begin
+                    peg2_count <= peg2_count - 1'b1;
+                    tmp_disk <= peg2[peg2_count + 1];
+                    peg1_count <= peg1_count + 1'b1;
+                    peg1[peg1_count] <= tmp_disk;
+                end
+        endcase
+    end
+endfunction
+
+function bit is_legal_move;
+    input [1:0] from, to;
+    begin
+        is_legal_move = 1'b0;
+
+        case ({from, to})
+            MOVE_FROM_PEG0_TO_PEG1: is_legal_move = (peg0[peg0_count] < peg1[peg1_count]);
+            MOVE_FROM_PEG0_TO_PEG2: is_legal_move = (peg0[peg0_count] < peg2[peg2_count]);
+            MOVE_FROM_PEG1_TO_PEG0: is_legal_move = (peg1[peg1_count] < peg0[peg0_count]);
+            MOVE_FROM_PEG1_TO_PEG2: is_legal_move = (peg1[peg1_count] < peg2[peg2_count]);
+            MOVE_FROM_PEG2_TO_PEG0: is_legal_move = (peg2[peg2_count] < peg0[peg0_count]);
+            MOVE_FROM_PEG2_TO_PEG1: is_legal_move = (peg2[peg2_count] < peg1[peg1_count]);
+        endcase
+    end
+endfunction
+
+default clocking
+    @(posedge clk);
+endclocking
+
+default disable iff (rst);
+
+assume_legal_move: assume property (is_legal_move(from_peg, to_peg) == 1'b1);
+
+cover property (peg0_count == '1);
+cover property (peg0_count == '0);
+cover property (peg1_count == '0);
+cover property (peg2_count == '0);
 
 endmodule
